ca094f34accb7a0fdbd47d70fb273e628d90ce0e
cryptic0053 <anirbanargha0053@gmail.com>
2024-02-02 11:21:06 +0600

    4

--- script.js
+++ script.js
@@ -4,3 +4,4 @@ MenuBtn.addEventListener('click',function(e){
     document.querySelector('body').classList.toggle('mobile-nav-active');
     this.classList.toggle('fa-xmark');
 })
+let typed=new Typed("auto-input")
